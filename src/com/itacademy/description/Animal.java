2a5dac234d04b3c72640de7d3c1c8cf5421d6114
Mikhail <L3v1s@mail.ru>
2020-09-05 14:39:24 +0300

    Add String "Hello all jungles...."

--- src/com/itacademy/description/Animal.java
+++ src/com/itacademy/description/Animal.java
@@ -3,5 +3,6 @@ package com.itacademy.description;
 public class Animal {
     public static void main(String[] args) {
         System.out.println("Hello jungles!");
+        System.out.println("Hello all jungles in the universe");
     }
 }
