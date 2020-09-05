7914051134a611a2b836286d210778094f45172e
Mikhail <L3v1s@mail.ru>
2020-09-05 14:53:39 +0300

    Add changes to String "Hello to all..."

--- src/com/itacademy/description/Animal.java
+++ src/com/itacademy/description/Animal.java
@@ -4,5 +4,6 @@ public class Animal {
     public static void main(String[] args) {
         System.out.println("Hello jungles!");
         System.out.println("Hello all jungles in the universe");
+        System.out.println("Hello to all animals in the universe");
     }
 }
